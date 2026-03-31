7437e99937eeb363ee16079571f36ff14ceda8a4
eclipsesolaire <djibybadiaga971@gmail.com>
2026-03-31 17:46:05 +0200

    Fix import path for Navbar component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import Navbar from '../nav/navbar'
+import Navbar from '../nav/Navbar'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
